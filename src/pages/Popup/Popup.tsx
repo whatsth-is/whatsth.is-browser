bc95d9e199636e9a3befcfba347a123bc8f09b9e
soup-bowl <code@soupbowl.io>
2023-02-27 22:35:36 +0000

    aaa

---
+++ src/pages/Popup/Popup.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Popup.css';
+
+const Popup: React.FC = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React!
+				</a>
+			</header>
+		</div>
+	);
+};
+
+export default Popup;
