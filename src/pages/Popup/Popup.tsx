b896e8ff2437b150b2716695ff2ed6349f0357b7
soup-bowl <code@soupbowl.io>
2023-03-01 22:17:54 +0000

    UX improvements.

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,3 +1,4 @@
+import { Link, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { InspectonResult } from '../inspection';
 import Layout from '../_layout';
@@ -19,6 +20,9 @@ export default function Popup() {
 	return (
 		<Layout>
 			<InspectonResult url={currentTabUrl} />
+			<Typography sx={{ fontSize: 14, textAlign: 'center' }} color="text.secondary">
+				With love from <Link href="https://github.com/soup-bowl/whatsth.is-browser" target="_blank" rel="noopener" color="text.secondary">soup-bowl</Link>
+			</Typography>
 		</Layout>
 	);
 }
--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, Container, createTheme, CssBaseline, styled, ThemeProvider, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Container, createTheme, CssBaseline, Link, styled, ThemeProvider, Toolbar, Typography } from '@mui/material';
 import { purple } from '@mui/material/colors';
 import { ReactNode, useMemo } from 'react';
 
@@ -37,25 +37,36 @@ export default function Layout({children}:Props) {
 		justifyContent: 'flex-end',
 	}));
 
+	const Main = styled('main')(({ theme }) => ({
+		flexGrow: 1,
+		padding: theme.spacing(0),
+	}));
+
 	return (
 		<ThemeProvider theme={theme}>
 			<Box sx={{ display: 'flex' }}>
 				<CssBaseline enableColorScheme />
-				<AppBar
-					sx={{
-						backgroundColor: theme.palette.primary.main,
-					}}
-				>
+				<AppBar sx={{ backgroundColor: theme.palette.primary.main }}>
 					<Toolbar>
-						<Typography variant="h6" noWrap component="div">What's this?</Typography>
+						<Typography variant="h6" noWrap component="div">
+							<Link
+								href="https://whatsth.is"
+								target="_blank"
+								rel="noopener"
+								color="inherit"
+								underline="none"
+							>
+								What's this?
+							</Link>
+						</Typography>
 					</Toolbar>
 				</AppBar>
-				<main>
+				<Main>
 					<DrawerHeader />
 					<Container maxWidth="md">
 						{children}
 					</Container>
-				</main>
+				</Main>
 			</Box>
 		</ThemeProvider>
 	);
